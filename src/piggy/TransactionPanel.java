23a59939aa6e90b12881be93724939a180395894
Andrew Brown <adb1413@rit.edu>
2011-03-28 19:22:07 -0400

    TransactionParser reads transactions from files

--- src/piggy/TransactionPanel.java
+++ src/piggy/TransactionPanel.java
@@ -2,9 +2,6 @@ package piggy;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JScrollPane;
@@ -26,45 +23,6 @@ public class TransactionPanel extends Box {
     }
     
     void load(File file) throws FileNotFoundException {
-        
-        List<Transaction> transactions = new LinkedList<Transaction>();
-        Scanner scanner = new Scanner(file);
-        Transaction transaction;
-        String line, value;
-        char key;
-        
-        scanner.nextLine();
-        
-        transaction = new Transaction();
-        while (scanner.hasNextLine()) {
-            line = scanner.nextLine();
-            key = line.charAt(0);
-            value = line.substring(1);
-            if (key == '^') {
-                transactions.add(transaction);
-                transaction = new Transaction();
-            } else {
-                apply(transaction, key, value);
-            }
-        }
-        model.add(transactions);
-    }
-    
-    void apply(Transaction transaction, char key, String value) {
-        switch (key) {
-        case 'D':
-            transaction.setDate(value);
-            break;
-        case 'T':
-            transaction.setAmount(value);
-            break;
-        case 'P':
-            transaction.setDescription(value);
-            break;
-        }
-    }
-    
-    static String getLine(Scanner scanner, int i) {
-        return scanner.nextLine().substring(i);
+        model.add(TransactionParser.parse(file));
     }
 }
---
+++ src/piggy/TransactionParser.java
@@ -0,0 +1,69 @@
+package piggy;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+/**
+ * Utility for loading transactions from a file.
+ */
+public class TransactionParser {
+    
+    /**
+     * Parses a file into transactions.
+     * 
+     * @param file File in QIF format
+     * @return Transactions loaded from file
+     * @throws FileNotFoundException if file does not exists
+     */
+    static List<Transaction> parse(File file) throws FileNotFoundException {
+        
+        List<Transaction> transactions = new ArrayList<Transaction>();
+        Scanner scanner = new Scanner(file);
+        Transaction transaction = new Transaction();
+        String line, value;
+        char key;
+        
+        scanner.nextLine();
+        while (scanner.hasNextLine()) {
+            line = scanner.nextLine();
+            key = line.charAt(0);
+            value = line.substring(1);
+            if (key != '^') {
+                apply(transaction, key, value);
+            } else {
+                transactions.add(transaction);
+                transaction = new Transaction();
+            }
+        }
+        return transactions;
+    }
+    
+    //--------------------------------------------------
+    // Helpers
+    //
+    
+    /**
+     * Stores a value in a transaction.
+     * 
+     * @param transaction Existing transaction
+     * @param k Key
+     * @param v Value
+     */
+    private static void apply(Transaction transaction, char k, String v) {
+        switch (k) {
+        case 'D':
+            transaction.setDate(v);
+            break;
+        case 'T':
+            transaction.setAmount(v);
+            break;
+        case 'P':
+            transaction.setDescription(v);
+            break;
+        }
+    }
+}
