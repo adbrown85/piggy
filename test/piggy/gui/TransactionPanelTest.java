720b70a06b3eec8eff13b42a2c5882237c200a27
Andy Brown <adb1413@rit.edu>
2011-03-29 00:16:45 -0400

    Tests use JTestFrame from jtesttools

--- test/piggy/gui/TransactionPanelTest.java
+++ test/piggy/gui/TransactionPanelTest.java
@@ -2,7 +2,7 @@ package piggy.gui;
 
 import java.io.File;
 import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import org.jtesttools.JTestFrame;
 import org.junit.Test;
 import piggy.gui.TransactionPanel;
 
@@ -12,21 +12,13 @@ public class TransactionPanelTest {
     @Test
     public void testShow() throws Exception {
         
-        final JFrame frame = new JFrame();
+        final JFrame frame = new JTestFrame();
         final TransactionPanel tp = new TransactionPanel();
         final File file = new File("test/sample.qif");
         
         tp.load(file);
         frame.add(tp);
         frame.pack();
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                frame.setVisible(true);
-            }
-        });
-        synchronized (frame) {
-            frame.wait();
-        }
+        JTestFrame.run(frame);
     }
 }
